7982a5295109eda81f55a4bf564100d0e5179826
abhijeet1720 <abhijeet17081@gmail.com>
2024-12-10 09:27:08 +1100

    Refactored code into MVC architecture

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const tasksRoute = require('./routes/tasks');
+const taskRoutes = require('./routes/taskRoutes');
+const connectDB = require('./config/db');
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use('/tasks', tasksRoute);
-
 // Connect to MongoDB
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.static('views'));
+
+// Routes
+app.use('/tasks', taskRoutes);
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
